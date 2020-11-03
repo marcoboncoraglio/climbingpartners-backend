62e40c694059391c03b8b75b8503d1af0994d2b3
Marco Boncoraglio <marcoboncoraglio@gmail.com>
2020-11-04 00:55:27 +0100

    update authroutes to produce jwt instead of session

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,15 +1,16 @@
-import express from "express";
-import passport from "passport";
+import express from 'express';
+import passport from 'passport';
 
-import UserLogin from "../models/userLogin";
-import FriendLists from "../models/friendLists";
-import UserCard from "../models/userCard";
-import UserDetails from "../models/userDetails";
-import Location from "../models/location";
+import UserLogin from '../models/userLogin';
+import FriendLists from '../models/friendLists';
+import UserCard from '../models/userCard';
+import UserDetails from '../models/userDetails';
+import Location from '../models/location';
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
-router.post("/register", (req: any, res: any, next: any) => {
+router.post('/register', (req: any, res: any, next: any) => {
   UserLogin.register(
     new UserLogin({ username: req.body.username }),
     req.body.password,
@@ -43,34 +44,36 @@ router.post("/register", (req: any, res: any, next: any) => {
         res.status(400).json({ error: err.message });
       }
 
-      passport.authenticate("local")(req, res, () => {
-        req.session.save((err: any) => {
-          if (err) {
-            return next(err);
-          }
-
-          res.status(201).json({ id: account._id });
-        });
+      const token = jwt.sign({ id: account._id }, process.env.TOKEN_SECRET, {
+        expiresIn: '1800s',
       });
+
+      res.status(201).json(token);
     }
   );
 });
 
-router.post("/login", (req: any, res: any, next: any) => {
-  passport.authenticate("local", (hashErr, user, info) => {
+router.post('/login', (req: any, res: any) => {
+  passport.authenticate('local', { session: false }, (hashErr, user, info) => {
     if (hashErr) {
       return res.status(401).json({ error: hashErr });
     }
     if (!user) {
       return res.status(401).json({ error: info.message });
     }
-    req.login(user, (err: any) => {
+
+    req.login(user, { session: false }, (err: any) => {
       if (err) {
-        return next(err);
+        console.log(err);
       }
-      res.status(200).json({ id: user._id });
+
+      const token = jwt.sign({ id: user._id  }, process.env.TOKEN_SECRET, {
+        expiresIn: '1800s',
+      });
+
+      res.json(token);
     });
-  })(req, res, next);
+  })(req, res);
 });
 
 /*
@@ -83,17 +86,5 @@ router.get('/google/callback',
         res.redirect('/');
     });
 */
-
-router.post("/logout", (req: any, res: any, next: any) => {
-  req.session.destroy((err: any) => {
-    if (err) {
-      return next(err);
-    }
-  });
-
-  req.logout();
-
-  res.status(200).send("Logged out");
-});
 
 export default router;
